badc1f9e6e59caac8b8b1765fb2b68677ba5700f
Jon Davis <jon@snowulf.com>
2015-09-13 15:42:06 -0700

    Add jq & built most of core code base. Looking fairly solid
    Also built for iOS

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,51 +1,81 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // Uses cordova-plugin-statusbar to keep the mobile OS status bar.
+        StatusBar.overlaysWebView(false);
+        StatusBar.backgroundColorByHexString("#333");
+
+        // Enable geolocation
+        navigator.geolocation.watchPosition(onSuccess, onError);
+    },
+    receivedEvent: function(id) {
 
         console.log('Received Event: ' + id);
     }
 };
 
 app.initialize();
+
+
+// Use cordova-plugin-geolocation for GPS information
+// Display information into the webview
+var onSuccess = function(position) {
+    $('#wherearewe').html('<pre>-You are at-\n'+
+          'Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n</pre>');
+};
+
+function onError(error) {
+    $('#wherearewe').html('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+
+// Take the form and make an ajax request instead
+$('#formme').submit(function(){
+  var data = {"lang": $('#formme #language').val()};
+  //alert(JSON.stringify(data));
+  console.log("sumitting data");
+  $.ajax({
+    type       : "POST",
+    // The server side needs the appropriate CORS rules
+    // For AWS API Gateway, see http://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+    url        : "https://i5mjyjbny3.execute-api.us-west-2.amazonaws.com/prod/randomHelloWorld",
+    crossDomain: true,
+    beforeSend : function() {
+      $.mobile.loading('show');
+      $('#formme #submitbtn').prop("disabled",true);
+    },
+    complete   : function() {
+      $.mobile.loading('hide');
+      $('#formme #submitbtn').prop("disabled",false);
+    },
+    data       : JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType   : 'json',
+    success    : function(response) {
+        $('#results ul').prepend("<li>"+response[0]+"="+response[1]+"\</li>\n");
+    },
+    error      : function() {
+        console.error("error");
+        alert('Not working!');
+    }
+  });
+
+  // This keeps the default POST action from firing & reloading your app
+  event.preventDefault();
+});
--- www/js/index.js
+++ www/js/index.js
@@ -8,6 +8,13 @@ var app = {
     },
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        // Uses cordova-plugin-statusbar to keep the mobile OS status bar.
+        StatusBar.overlaysWebView(false);
+        StatusBar.backgroundColorByHexString("#333");
+
+        // Enable geolocation
+        navigator.geolocation.watchPosition(onSuccess, onError);
     },
     receivedEvent: function(id) {
 
@@ -16,3 +23,59 @@ var app = {
 };
 
 app.initialize();
+
+
+// Use cordova-plugin-geolocation for GPS information
+// Display information into the webview
+var onSuccess = function(position) {
+    $('#wherearewe').html('<pre>-You are at-\n'+
+          'Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n</pre>');
+};
+
+function onError(error) {
+    $('#wherearewe').html('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+
+// Take the form and make an ajax request instead
+$('#formme').submit(function(){
+  var data = {"lang": $('#formme #language').val()};
+  //alert(JSON.stringify(data));
+  console.log("sumitting data");
+  $.ajax({
+    type       : "POST",
+    // The server side needs the appropriate CORS rules
+    // For AWS API Gateway, see http://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+    url        : "https://i5mjyjbny3.execute-api.us-west-2.amazonaws.com/prod/randomHelloWorld",
+    crossDomain: true,
+    beforeSend : function() {
+      $.mobile.loading('show');
+      $('#formme #submitbtn').prop("disabled",true);
+    },
+    complete   : function() {
+      $.mobile.loading('hide');
+      $('#formme #submitbtn').prop("disabled",false);
+    },
+    data       : JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType   : 'json',
+    success    : function(response) {
+        $('#results ul').prepend("<li>"+response[0]+"="+response[1]+"\</li>\n");
+    },
+    error      : function() {
+        console.error("error");
+        alert('Not working!');
+    }
+  });
+
+  // This keeps the default POST action from firing & reloading your app
+  event.preventDefault();
+});
